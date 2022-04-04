efa1f4fe6a7dc51393499bcfe6dd04d91a2f7595
Eliachar Feig <efeig15@gmail.com>
2022-04-04 19:12:36 +0300

    Update main.py

--- ex2/main.py
+++ ex2/main.py
@@ -2,7 +2,13 @@ import functools
 from typing import Iterator
 
 
-def generate_all_perfect_dividers(divider: int) -> Iterator[int]:
+def generate_all_perfect_dividers(divider: int = 2) -> Iterator[int]:
+    """
+    Generator that generates all positive integers greater(or equals) to the integer passed to
+    the function, that the sum of their divisors are equals to them ("perfect dividers").
+    :param divider: The start integer
+    :return: An iterator of the "perfect dividers"
+    """
     while True:
         if divider == functools.reduce(lambda total, new_div: total + new_div,
                                        filter(lambda num: divider % num == 0, range(1, divider // 2 + 1))):
@@ -11,6 +17,9 @@ def generate_all_perfect_dividers(divider: int) -> Iterator[int]:
 
 
 def print_all_dividers_equals_to_sum_of_their_divisors() -> None:
+    """
+    Print all positive integers that the sum of their divisors are equals to them
+    """
     for perfect_divider in generate_all_perfect_dividers(6):
         print(perfect_divider)
 
